bde67e4947aab26452a0d61a862163a855eaa669
Bozuma Acquaah <bozuma.acquaah@gmail.com>
2018-09-14 01:02:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
-var uppercase = "HELLO"
 function logShout(string) {
-  console.log('hello'.toUpperCase)
+   console.log(string.toUppercase) 
+  
 }
 
 function logWhisper(string) {
-  console.log('HELLO'.toLowerCase)
+  console.log(string.toLowerCase)
 }
 
 function sayHiToGrandma(string) {
-  return string.toLowerCase("I can't hear you!")
-}
-
-function sayHiToGrandma(string) {
-  return string.toUpperCase("YES INDEED!")
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+   if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+   if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
